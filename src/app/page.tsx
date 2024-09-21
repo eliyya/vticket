6a32578cc8646061fa3bde86fe5006965a472e10
eliyya <tecno_eli@hotmail.com>
2024-09-21 14:19:23 -0600

    add flavors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
+'use client'
 import localFont from 'next/font/local'
 import BGHero from '@/img/music-background-80s-neon-80-s-hd-wallpaper-preview.jpg'
-import Image from 'next/image'
+import BGBrick from '@/img/neon-light-brick-wall-texture-slide-effect-footage-4k.jpg'
 
 const RodRangue = localFont({
     src: './fonts/Road_Rage.otf',
@@ -11,6 +12,21 @@ const Commando = localFont({
     src: './fonts/commando.ttf',
     weight: '100 900',
 })
+
+function VHSEffect() {
+    return (
+        <div className="absolute overflow-hidden h-full w-full pointer-events-none">
+            <video
+                autoPlay
+                loop
+                muted
+                className="mix-blend-difference h-full w-full  object-cover"
+            >
+                <source src="/VHS-Background_Noisy.mp4" type="video/mp4" />
+            </video>
+        </div>
+    )
+}
 
 export default function Home() {
     return (
@@ -23,19 +39,7 @@ export default function Home() {
                 }}
                 className={`h-screen w-full flex flex-col justify-center items-center relative`}
             >
-                <div className="absolute overflow-hidden h-full w-full">
-                    <video
-                        autoPlay
-                        loop
-                        muted
-                        className="mix-blend-difference h-full w-full  object-cover"
-                    >
-                        <source
-                            src="/VHS-Background_Noisy.mp4"
-                            type="video/mp4"
-                        />
-                    </video>
-                </div>
+                <VHSEffect />
                 <h1
                     className={`${RodRangue.className} text-9xl px-5 -rotate-12 bg-clip-text text-transparent bg-gradient-to-tr to-fuchsia-500 via-red-500 from-yellow-500`}
                 >
@@ -63,7 +67,14 @@ export default function Home() {
                     </h1>
                 </div>
             </header>
-            <main>Obtener mi ticket</main>
+            <section
+                style={{
+                    backgroundImage: `url(${BGBrick.src})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                }}
+                className="h-screen w-screen flex flex-col justify-center items-center relative"
+            ></section>
         </div>
     )
 }
